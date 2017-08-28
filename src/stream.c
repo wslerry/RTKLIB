a774d61cb829546d7ba34c0a88c3675bdb9842b0
Egor Fedorov <egor.fedorov@emlid.com>
2017-08-28 17:54:25 +0300

    stream: add baud rate 460800

--- src/stream.c
+++ src/stream.c
@@ -330,7 +330,7 @@ static DWORD WINAPI serialthread(void *arg)
 static serial_t *openserial(const char *path, int mode, char *msg)
 {
     const int br[]={
-        300,600,1200,2400,4800,9600,19200,38400,57600,115200,230400
+        300,600,1200,2400,4800,9600,19200,38400,57600,115200,230400,460800
     };
     serial_t *serial;
     int i,brate=9600,bsize=8,stopb=1,tcp_port=0;
@@ -342,7 +342,7 @@ static serial_t *openserial(const char *path, int mode, char *msg)
     char dcb[64]="";
 #else
     const speed_t bs[]={
-        B300,B600,B1200,B2400,B4800,B9600,B19200,B38400,B57600,B115200,B230400
+        B300,B600,B1200,B2400,B4800,B9600,B19200,B38400,B57600,B115200,B230400,B460800
     };
     struct termios ios={0};
     int rw=0;
